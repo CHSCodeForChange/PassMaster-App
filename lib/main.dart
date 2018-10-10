6f7b819881648041b0ef210d7bf9d602220b9a3d
jpaav <joepaavola@gmail.com>
2018-10-10 17:47:54 -0400

    Fixed bugs stemming from flutter upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import './pages/main/main.dart';
 import './pages/auth/login.dart';
 
 
-void main() {
-  runApp(new MaterialApp(
-    home: new Login(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login()
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_proj/main.dart';
+import 'package:pass_master/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
