5b5e9fb728b8ecde0c9d516870ca0d9c010699c7
jpaav <joepaavola@gmail.com>
2018-10-11 10:49:38 -0400

    Added icons to pass-mini

--- lib/fragments/pass/pass-mini.dart
+++ lib/fragments/pass/pass-mini.dart
@@ -10,6 +10,16 @@ class PassMini extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Icon icon;
+    if(pass.type == "SRTPass") {
+      icon = new Icon(Icons.hourglass_full);
+    }
+    else if(pass.type == "LocationPass") {
+      icon = new Icon(Icons.location_on);
+    }
+    else if(pass.type == "TeacherPass") {
+      icon = new Icon(Icons.person);
+    }
     return new Card(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
       child: new Container(
@@ -17,10 +27,19 @@ class PassMini extends StatelessWidget {
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            new Text(
-              pass.destination,
-              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+            new Row(
+              children: <Widget>[
+                new Container(
+                  child: icon,
+                  padding: EdgeInsets.only(right: 10.0),
+                ),
+                new Text(
+                  pass.destination,
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
+
             new Text(
               pass.getDateDuration(),
               style: TextStyle(fontSize: 17.0),
