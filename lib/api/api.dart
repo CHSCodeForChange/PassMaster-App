6f305bfa9096ea56e5da7ca4a0ea0ebfdd699014
Armaan Goel <armaangoel78@gmail.com>
2019-03-17 14:09:20 -0400

    moved to deployment version

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,4 +1,4 @@
 abstract class API {
   //change to false for debug mode.
-  String baseUrl = false ? "https://passmaster.herokuapp.com/rest/" : "http://127.0.0.1:8000/rest/";
+  String baseUrl = true ? "https://passmaster.herokuapp.com/rest/" : "http://127.0.0.1:8000/rest/";
 }
