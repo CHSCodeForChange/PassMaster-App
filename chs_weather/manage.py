216d159cf329c5c42eaf8b8464601cad22509d50
jpaav <joepaavola@gmail.com>
2019-04-15 12:09:59 -0400

    Initial commit

--- chs_weather/manage.py
+++ chs_weather/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chsweather.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chs_weather.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
